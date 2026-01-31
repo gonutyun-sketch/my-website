8b26bbefde3a3db62c3acc313ca29b645e0e1f8d
gonutyun-sketch <gonutyun@gmail.com>
2026-02-01 01:10:02 +0900

    Add files via upload

---
+++ 내웹사이트/script.js
@@ -0,0 +1,5 @@
+// 문의 버튼 클릭 시 카카오톡 상담 링크로 이동
+document.getElementById("chatBtn").addEventListener("click", function() {
+    // 실제 상담 링크로 바꾸세요
+    window.open("https://pf.kakao.com/_abcdefg/chat", "_blank");
+});
