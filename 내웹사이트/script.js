60a6291a92daf2344e90d3ba5261dbe1b5b8ceef
gonutyun-sketch <gonutyun@gmail.com>
2026-02-01 01:08:39 +0900

    Delete 내웹사이트 directory

--- 내웹사이트/script.js
+++
@@ -1,5 +0,0 @@
-// 문의 버튼 클릭 시 카카오톡 상담 링크로 이동
-document.getElementById("chatBtn").addEventListener("click", function() {
-    // 실제 상담 링크로 바꾸세요
-    window.open("https://pf.kakao.com/_abcdefg/chat", "_blank");
-});
